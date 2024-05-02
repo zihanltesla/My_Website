3cf325ade186cf3a9af8afaa4bf740134b5de77d
Zihan Liu <zihanliu@zihandemacbook-pro.home>
2024-05-02 12:40:59 +0200

    add project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,7 +62,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>  
+            {/* <Nav.Item>  
               <Nav.Link
                 as={Link}
                 to="/project"
@@ -73,7 +73,7 @@ function NavBar() {
                 />{" "}
                 Projects
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item>
               <Nav.Link
