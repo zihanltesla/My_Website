2aaaa3056b0dbec57d279609771ff6308367d89f
zihanltesla <liu3675716@gmail.com>
2025-09-11 17:01:32 +0200

    remove early post and adjust mobile view

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -51,7 +51,7 @@ export default function About() {
     },
   ];
   return (
-    <Column maxWidth="m">
+    <Column maxWidth="m" className={styles.mobileContainer}>
       <Schema
         as="webPage"
         baseURL={baseURL}
